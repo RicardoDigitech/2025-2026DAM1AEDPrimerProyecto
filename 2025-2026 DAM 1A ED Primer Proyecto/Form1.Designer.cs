c3a61293365b75541b6c60654951d003f3825122
RicardoDigitech <ricardo.sornosa@digitechfp.com>
2025-10-03 09:41:32 +0200

    Agregar archivos de proyecto.

---
+++ 2025-2026 DAM 1A ED Primer Proyecto/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace _2025_2026_DAM_1A_ED_Primer_Proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBoton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBoton
+            // 
+            this.btnBoton.Location = new System.Drawing.Point(268, 137);
+            this.btnBoton.Name = "btnBoton";
+            this.btnBoton.Size = new System.Drawing.Size(75, 23);
+            this.btnBoton.TabIndex = 0;
+            this.btnBoton.Text = "button1";
+            this.btnBoton.UseVisualStyleBackColor = true;
+            this.btnBoton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBoton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBoton;
+    }
+}
+
